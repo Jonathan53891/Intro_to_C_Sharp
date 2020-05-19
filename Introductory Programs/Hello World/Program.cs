e8ef1cf030c7adc98c9715760f65097c25c5704c
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-19 13:27:35 -0500

    Update Program.cs

--- Introductory Programs/Hello World/Program.cs
+++ Introductory Programs/Hello World/Program.cs
@@ -9,6 +9,12 @@ namespace Giraffe
             Console.WriteLine("Hello World!");
 
             Console.ReadLine();
+            /*
+            Console.ReadLine() gives us the ability to view the console that displays
+            the program being run. This is more relevant if Visual Studio is being run 
+            on Windows, since I didn't have this issue with my Mac. Regardless, this
+            simple line of code is pretty helpful nonetheless.
+            */
         }
     }
 }
