887bcc3b520506d5007c7fddedea3a458ed4d27b
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-19 00:36:42 -0500

    Update Program.cs

--- Introductory Programs/Hello World/Program.cs
+++ Introductory Programs/Hello World/Program.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace Giraffe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
