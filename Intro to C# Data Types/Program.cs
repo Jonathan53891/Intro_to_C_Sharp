6916f52b6c6f115f9ddbfa0281c63f48c97f159f
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-18 23:36:45 -0500

    Create Program.cs

---
+++ Intro to C# Data Types/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Giraffe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3;
+            bool isMale = true;
+
+            Console.WriteLine("Hello");
+
+            Console.ReadLine();
+        }
+    }
+}
