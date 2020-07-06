fbe940300952d1843a75e64d0636a612b70a7229
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-07-05 19:55:50 -0500

    Update Program.cs

--- Week 1 Assignment/Program.cs
+++ Week 1 Assignment/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using static System.Console;
 
 namespace Programming_Assignment_1
 {
@@ -6,45 +7,45 @@ namespace Programming_Assignment_1
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter the first integer: ");
+            Write("Enter the first integer: ");
 
-            //int num1 = Console.ReadLine();
+            //int num1 = ReadLine();
             /*
              * If we use the line above, we will receive an error because
-             * Console.ReadLine() takes in the user input as a string. So we
-             * must convert the user input from a string to an integer.
+             * ReadLine() takes in the user input as a string. So we must
+             * convert the user input from a string to an integer.
              */
 
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num1 = Convert.ToInt32(ReadLine());
             /* 
              * The program will run now, since the string gets converted
              * to an integer
              */
 
-            Console.Write("Enter the second integer: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            Write("Enter the second integer: ");
+            int num2 = Convert.ToInt32(ReadLine());
 
-            Console.WriteLine("The two numbers you entered are: " + num1 + " and " + num2);
-            Console.WriteLine("The sum of the two numbers is " + (num1 + num2)); //adds the two inputted numbers
-            Console.WriteLine("The difference of the two numbers is " + (num1 - num2));
-            Console.WriteLine("The product of the two numbers is " + (num1 * num2));
-            Console.WriteLine("The quotient of the two numbers is " + (num1 / num2));
-            Console.WriteLine("The remainder of the two numbers is " + (num1 % num2));
-            Console.WriteLine(""); //for spacing
-            Console.WriteLine("The larger of the two numbers is " + Math.Max(num1, num2));
+            WriteLine("The two numbers you entered are: " + num1 + " and " + num2);
+            WriteLine("The sum of the two numbers is " + (num1 + num2));
+            WriteLine("The difference of the two numbers is " + (num1 - num2));
+            WriteLine("The product of the two numbers is " + (num1 * num2));
+            WriteLine("The quotient of the two numbers is " + (num1 / num2));
+            WriteLine("The remainder of the two numbers is " + (num1 % num2));
+            WriteLine(""); //for spacing
 
             if (num1 == num2)
             {
-                Console.WriteLine("Both numbers are the same.");
+                WriteLine("Both numbers are the same.");
             }
             else
             {
-                Console.WriteLine("Given numbers are NOT the same.");
+                WriteLine("Given numbers are NOT the same.");
+                WriteLine("The larger of the two numbers is " + Math.Max(num1, num2));
             }
 
-            Console.WriteLine("Press the [enter] key to continue");
+            WriteLine("Press the [enter] key to continue");
 
-            Console.ReadLine();
+            ReadLine();
         }
     }
 }
